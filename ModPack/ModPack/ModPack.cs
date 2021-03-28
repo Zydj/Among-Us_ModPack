0396500769f2366ef8b578db8b787fff71b68bb3
Zydj <charles.desjardins96@gmail.com>
2021-03-27 21:06:09 -0400

    Initial commit

---
+++ ModPack/ModPack/ModPack.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.IL2CPP;
+using HarmonyLib;
+using Reactor;
+
+namespace ModPack
+{
+    [BepInPlugin(Id)]
+    [BepInProcess("Among Us.exe")]
+    [BepInDependency(ReactorPlugin.Id)]
+    public class ModPack : BasePlugin
+    {
+        public static bool debug = true;
+
+        public const string Id = "org.bepinex.plugins.ModPack";
+        public static BepInEx.Logging.ManualLogSource log;
+
+        public static bool introDone = false;
+
+        public Harmony Harmony { get; } = new Harmony(Id);
+
+        public ConfigEntry<string> Name { get; private set; }
+
+        public override void Load()
+        {
+            log = Log;
+            log.LogMessage("Mod Pack Loaded");
+
+            Harmony.PatchAll();
+        }        
+    }
+}
